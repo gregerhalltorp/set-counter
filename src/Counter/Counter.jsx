d4bab66d73afd8cb7d79529cf1b476497a944eb3
Greger Hälltorp <greger.halltorp@gmail.com>
2018-02-22 08:35:31 +0700

    show name, style improvements

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Counter = ({ exercise }) => {
-  const { reps } = exercise;
+  const { reps, name } = exercise;
   return (
     <div className="sc-counter">
+      <div className="sc-counter__heading">{name}</div>
       <button className="sc-counter__button">{reps}</button>
     </div>
   );
